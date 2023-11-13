bdbde4a397b0b444f200c5c6730413dbd3342d1a
David Steiner <david1.steiner@gmail.com>
2023-11-13 20:16:08 +0100

    Completed exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farTemp) {
+  let celcTemp = (farTemp - 32) * 5/9
+  return Math.round(celcTemp * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcTemp) {
+  let farTemp = (celcTemp * 9/5) + 32
+  return Math.round(farTemp * 10) / 10
 };
 
 // Do not edit below this line
