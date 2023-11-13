bebe9530384dc535602b5974812dd5a66f19482c
David Steiner <david1.steiner@gmail.com>
2023-11-13 16:49:58 +0100

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringReversed = '';
 
+    for (i = string.length; i > 0; --i) {
+        stringReversed += string.substring(i - 1, i)
+    }
+    return stringReversed
 };
 
 // Do not edit below this line
