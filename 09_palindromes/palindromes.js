ab77db4c8d40874d2b9496b9e2350967ea21f0ce
David Steiner <david1.steiner@gmail.com>
2023-11-28 15:33:35 +0100

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lowerCaseStr = string.toLowerCase();
+    const nonAlphNum = lowerCaseStr.replace(/\W/g, '');
+    const splitString = nonAlphNum.split('');
+    const palindrome = splitString.reverse().join('');
 
+    console.log(nonAlphNum, palindrome);
+    return nonAlphNum === palindrome;
 };
 
 // Do not edit below this line
