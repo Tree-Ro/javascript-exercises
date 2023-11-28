dfa7011d6a5d94e515eb99c8a4eb5e4331f381dc
David Steiner <david1.steiner@gmail.com>
2023-11-28 16:01:51 +0100

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (Math.sign(+n) === -1) {
+        return 'OOPS';
+    }
 
+    const PHI = (1 + Math.sqrt(5)) / 2;
+    const nTerm = (PHI ** +n - (1 - PHI) ** +n) / Math.sqrt(5);
+    return Math.floor(nTerm);
 };
 
 // Do not edit below this line
