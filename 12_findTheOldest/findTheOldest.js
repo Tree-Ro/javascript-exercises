6d361cf9b0d42903d62506944a978818e4814250
David Steiner <david1.steiner@gmail.com>
2023-11-28 20:19:50 +0100

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    arr.forEach((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+    });
 
+    arr.sort((a, b) => b.age - a.age); //Sort oldest to youngest
+    return arr[0]; //Return oldest
 };
 
 // Do not edit below this line
