a0b56f71de0beb3bca91300ecde005f1bbd03d28
David Steiner <david1.steiner@gmail.com>
2023-11-28 14:39:30 +0100

    Completed 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+    return args.reduce((partialSum, i) => partialSum + i, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+    return args.reduce((partialSum, i) => partialSum * i, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+    return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let total = 1;
+    for (; a > 0; --a) {
+        total *= a;
+    }
+    return total;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
